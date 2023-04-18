3774b90a7ed890d06da29d6dff73aef33dd174e8
sawyerrr24 <sawyercage1212@gmail.com>
2023-04-18 13:15:55 +0000

    changes have been made

--- Advancedscript.js
+++ Advancedscript.js
@@ -133,34 +133,64 @@ function Person(fName,lName){
     this.lastName=lName
 }
 
-const person1=new Person('Bruce','Wayne')
-const person2=new Person('Clark','Kent')
-
 Person.prototype.getFullName=function(){
-    return this.firstName+' '+ this.lastName
-
+    return this.firstName+ ' ' + this.lastName
 }
 
-// console.log(person1.getFullName())
-// console.log(person2.getFullName())
-
-
 function SuperHero(fName,lName){
-    //this = {}
-    Person.call(this,fName,lName  )
-
+    Person.call(this,fName,lName)
     this.isSuperHero=true
-} 
-
+}
 
 SuperHero.prototype.fightCrime=function(){
     console.log('Fighting Crime');
-}                                     
+}
+
+
 SuperHero.prototype=Object.create(Person.prototype)
 
-const batman=new SuperHero('Bruce', 'Wayne')
-SuperHero.prototype.constructor=SuperHero
+const batman=new SuperHero('Bruce','Wayne')
+SuperHero.prototype.constructor= SuperHero
 console.log(batman.getFullName());
 
 
+
+
+//  const person1=new Person('Bruce','Wayne')
+// const person2=new Person('Clark','Kent')
+
+
+
+// console.log(person1.getFullName())
+//  console.log(person2.getFullName())
+
+
+
+
+
+
 //class
+// class person{
+//     constructor(fName,lName) {
+//         this.firstName = fName
+//         this.lastName = lName
+//     }
+//     sayMyName(){
+//         return this.firstName+' '+ this.lastName
+//     }
+// }
+
+// const classp1=new person('Spider','Man')
+// console.log(classp1.sayMyName())
+
+// class newSuperHero extends person{
+//     constructor(fName,lName){
+//     super(fName,lName)
+//     this.isSuperHero=true
+//     }
+//     fightCrime(){
+//         console.log('Fighting Crime');
+//     }
+// }
+// const batman=new newSuperHero('Bruce','Wayne')
+// console.log(batman.sayMyName())
